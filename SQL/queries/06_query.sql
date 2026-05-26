80efd0da1a6968708adc944882bedf67717ac445
Himanshu24 <hkanchap24@gmail.com>
2026-05-27 01:06:08 +0530

    Update 06_query.sql

--- SQL/queries/06_query.sql
+++ SQL/queries/06_query.sql
@@ -1,31 +1,12 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `heart_health_project`.`vw_double_burden_hotspots` AS
-    SELECT 
-        `heart_health_project`.`heart_health_project`.`city_name` AS `city_name`,
-        `heart_health_project`.`heart_health_project`.`location_tier` AS `location_tier`,
-        COUNT(0) AS `total_respondents`,
-        SUM((CASE
-            WHEN
-                ((`heart_health_project`.`heart_health_project`.`income_rank` = 1)
-                    AND (`heart_health_project`.`heart_health_project`.`air_quality_level` = 'Very Poor'))
-            THEN
-                1
-            ELSE 0
-        END)) AS `vulnerable_count`,
-        ROUND(((SUM((CASE
-                    WHEN
-                        ((`heart_health_project`.`heart_health_project`.`income_rank` = 1)
-                            AND (`heart_health_project`.`heart_health_project`.`air_quality_level` = 'Very Poor'))
-                    THEN
-                        1
-                    ELSE 0
-                END)) * 100.0) / COUNT(0)),
-                2) AS `double_burden_pct`
-    FROM
-        `heart_health_project`.`heart_health_project`
-    GROUP BY `heart_health_project`.`heart_health_project`.`city_name` , `heart_health_project`.`heart_health_project`.`location_tier`
-    HAVING (`double_burden_pct` > 5)
-    ORDER BY `double_burden_pct` DESC
+CREATE OR REPLACE VIEW vw_double_burden_hotspots AS
+SELECT 
+    city_name,
+    location_tier,
+    COUNT(*) AS total_respondents,
+    -- Using TRIM and UPPER to avoid matching issues
+    SUM(CASE WHEN income_rank = 1 AND TRIM(UPPER(air_quality_level)) = 'VERY POOR' THEN 1 ELSE 0 END) AS vulnerable_count,
+    ROUND((SUM(CASE WHEN income_rank = 1 AND TRIM(UPPER(air_quality_level)) = 'VERY POOR' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)),2) AS double_burden_pct
+FROM heart_health_project
+GROUP BY city_name, location_tier
+HAVING double_burden_pct > 10
+ORDER BY double_burden_pct DESC;
